e15b83943e7200226dc3f48ff60e4427bc199778
Jimmy Givans <jauyoma.dev@gmail.com>
2024-09-09 17:37:10 +0300

    feat: set a custom error message on password mismatch

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 const password: HTMLInputElement = document.querySelector("#password")!;
 const confirm: HTMLInputElement = document.querySelector("#confirm-password")!;
+const errMsg: HTMLSpanElement = document.querySelector("div > label > span")!;
 
 const validatePasswords = () => {
   const isValid =
@@ -10,6 +11,7 @@ const validatePasswords = () => {
   if (!isValid) {
     password.classList.add("border-red-500");
     confirm.classList.add("border-red-500");
+    errMsg.textContent = "* Passwords do not match";
   } else {
     password.classList.remove("border-red-500");
     confirm.classList.remove("border-red-500");
